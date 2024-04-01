404909d406dd122daea17f19c8f6555244b975d5
Daniel Schwenzfeur <daniel.schwenzfeur@gmail.com>
2024-04-01 14:07:22 +0200

    add systemd

--- setup.sh
+++ setup.sh
@@ -8,12 +8,12 @@ useradd -m crmr
 passwd crmr
 
 mkdir /scripts
-wget -P /scripts/ https://github.com/Crumar/server-setup/blob/main/scripts/firewall.sh
+wget -P /scripts/ https://raw.githubusercontent.com/Crumar/server-setup/main/scripts/firewall.sh
 chmod u+x /scripts/firewall.sh
 echo "@reboot root /scripts/firewall.sh" > /etc/crontab
 sed -i 's/Port 22/Port 22222/' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-apt install fail2ban nginx -y
+apt install fail2ban nginx htop -y
 
 
 ### STEAM ###
@@ -23,4 +23,11 @@ dpkg --add-architecture i386
 apt update
 apt install steamcmd -y
 useradd -m steam
-su -c "wget -P ~/scripts/ https://github.com/Crumar/server-setup/blob/main/scripts/firewall.sh " -g apache apache
+passwd steam
+su -c "mkdir ~/scripts" -g steam steam
+su -c "wget -P ~/scripts/ https://raw.githubusercontent.com/Crumar/server-setup/main/steam/scripts/_update_cs2" -g steam steam
+su -c "wget -P ~/scripts/ https://raw.githubusercontent.com/Crumar/server-setup/main/steam/scripts/autoupdate_cs2.sh" -g steam steam
+su -c "wget -P ~/scripts/ https://raw.githubusercontent.com/Crumar/server-setup/main/steam/scripts/update_cs2.sh" -g steam steam
+su -c "chmod u+x ~/scripts/*.sh" -g steam steam
+wget -P /etc/systemd/system/
+reboot
