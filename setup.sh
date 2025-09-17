6549759bdbcf0640e9c1247f0fbdbb475684d1ef
Crumar <daniel.schwenzfeur@gmail.com>
2025-09-17 09:33:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,9 +11,9 @@ mkdir /scripts
 wget -P /scripts/ https://raw.githubusercontent.com/Crumar/server-setup/main/scripts/firewall.sh
 chmod u+x /scripts/firewall.sh
 echo "@reboot root /scripts/firewall.sh" > /etc/crontab
-sed -i 's/Port 22/Port 22222/' /etc/ssh/sshd_config
+sed -i -E 's/[#]?Port 22/Port 22222/' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-apt install fail2ban nginx htop -y
+apt install fail2ban nginx htop net-tools dnsutils -y
 
 reboot
 
