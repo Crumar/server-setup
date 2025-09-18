9754e67b51d4145cca33bb169428825a5f9d366b
Crumar <dev@crumar.de>
2025-09-18 20:43:36 +0200

    update to user service

--- steam_cs2.sh
+++ steam_cs2.sh
@@ -12,8 +12,11 @@ su -c "wget -P ~/scripts/ https://raw.githubusercontent.com/Crumar/server-setup/
 su -c "wget -P ~/scripts/ https://raw.githubusercontent.com/Crumar/server-setup/main/steam/scripts/update_cs2.sh" -g steam steam
 su -c "wget -P ~/scripts/ https://raw.githubusercontent.com/Crumar/server-setup/main/steam/scripts/start_cs2.sh" -g steam steam
 su -c "chmod u+x ~/scripts/*.sh" -g steam steam
-wget -P /etc/systemd/system/ https://raw.githubusercontent.com/Crumar/server-setup/main/etc/systemd/system/cs2.service
-systemctl daemon-reload
-systemctl enabled cs2
+
+su -c "~/scripts/update_cs2.sh"
+
+wget -P ~/.config/systemd/user/cs2.service https://raw.githubusercontent.com/Crumar/server-setup/main/steam/.config/systemd/user/cs2.service
+su -c "systemctl --user daemon-reload"
+su -c "systemctl --user enabled cs2"
 #change token in start cs2.sh
-systemctl start cs2
+su -c "systemctl --user start cs2"
